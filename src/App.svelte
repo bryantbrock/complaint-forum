943f23c90dd40babda2dc26ed92e2245b2c0eb01
Bryant Brock <bryant.brock@bryant-brock.local>
2021-07-27 22:16:48 -0700

    modal creation, signin form

--- src/App.svelte
+++ src/App.svelte
@@ -5,25 +5,7 @@
   import Main from 'partials/Main.svelte';
   import Footer from 'partials/Footer.svelte';
   import Header from 'partials/Header.svelte';
-
-  onMount(async () => {
-    const email = 'bryantleebrock@gmail.com'
-    const password = '123'
-  
-    // 
-    let {records: [user]} = await Users({
-      params: {
-        'maxRecords': 1,
-        'filterByFormula': `SEARCH('${email}', {email})`,
-        'filterByFormula': `SEARCH('${password}', {password})`,
-        'fields%5B%5D': 'email'
-      }
-    });
-
-    if (!!user) {
-      console.log('logged in.')
-    }
-  })
+  import Signin from 'partials/Signin.svelte';
 </script>
 
 <Tailwind />
@@ -32,4 +14,7 @@
   <Header />
   <Main />
   <Footer />
+
+  <!-- Modal -->
+  <Signin />
 </div>
